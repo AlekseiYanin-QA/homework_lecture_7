3f7da8464a8d8d09c0315d1402e3ab9dbcde6a56
Yanin.A.V <aleksei.yanin.qa@gmail.com>
2024-02-04 11:35:22 +0300

    fix comments

--- tests/test_pozitiv_files.py
+++ tests/test_pozitiv_files.py
@@ -3,11 +3,14 @@ import os
 from pypdf import PdfReader
 from openpyxl import load_workbook
 
+
 def test_users_csv(create_archive):
     path = os.path.join(create_archive, 'csv.csv')
     with open(path) as csvfile:
         reader = csv.DictReader(csvfile, delimiter=';')
         names = [row['Username'] for row in reader]
+    assert names == ['booker12']
+
 
 def test_xlsx_file(create_archive):
     path = os.path.join(create_archive, 'file_example_XLSX_50.xlsx')
@@ -15,6 +18,7 @@ def test_xlsx_file(create_archive):
     sheet = open_xlsx.active
     name = sheet.cell(row=3, column=2).value
     assert name == 'Mara'
+
 
 def test_pdf_page_content(create_archive):
     path = f'{create_archive}/Python Testing with Pytest (Brian Okken).pdf'
